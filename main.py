c5765f91563aa45d59488683d2a1ee016b53002d
Róbert Tóth <robert.toth.ext@nokia.com>
2018-12-12 17:06:42 +0100

    2

--- main.py
+++ main.py
@@ -44,12 +44,12 @@ class Cell(object):
         #         self.nbs +=1
         # if generation[self.offset + 1].state == 1:
         #     print("YESH")
-        north = generation[(self.offset - ROWS) % (ROWS * COLUMNS)]
-        south = generation[(self.offset + ROWS) % (ROWS * COLUMNS)]
+        north = generation[get_offset(3,2)]
+        south = generation[(self.col + ROWS) % (ROWS * COLUMNS)]
         # i + 1 - ((i mod 8) div 7) * 8
-        west = generation[(self.offset + (ROWS- 1) + (ROWS * COLUMNS)) % (ROWS * COLUMNS)]
+        west = generation[(self.offset - 1 + (ROWS * COLUMNS)) % (ROWS * COLUMNS)]
         east = generation[(self.offset + COLUMNS +1) % (ROWS * COLUMNS)]
-        print(west)
+        print(north)
 
     def __str__(self):
         return "Cell, index=({}, {}), offset={}, state={}".format(self.row, self.col, self.offset, self.state)
@@ -73,7 +73,7 @@ first_generation = list(
 for i in first_generation:
     print((i.row, i.col, i.state))
 
-c1 = first_generation[4]
+c1 = first_generation[5]
 print("Selected cell: {}".format(c1))
 c1.count_nbs(first_generation)
 
