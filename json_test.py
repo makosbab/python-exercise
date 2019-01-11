dec208d04f5acad28d9c837d897f71eb07d8d4b2
Róbert Tóth <robert.toth.ext@nokia.com>
2019-01-11 16:09:55 +0100

    decorator

--- json_test.py
+++ json_test.py
@@ -1,123 +1,24 @@
-import json
+def reg(func):
+  return func() + 10
 
-szornyekJSON = '''
-    {
-      "nev": "Aboleth",
-      "meret": "Óriási",
-      "tipus": "Rendellenesség",
-      "tipus_modosito": "Vízi",
-      "tulajdonsagok": 
-      [
-        {
-          "nev" : "Erő",
-          "pont" : 26
-        },
-        {
-          "nev" : "Ügy",
-          "pont" : 12
-        },
-        {
-          "nev" : "Áll",
-          "pont" : 20
-        },
-        {
-          "nev" : "Int",
-          "pont" : 15
-        },
-        {
-          "nev" : "Böl",
-          "pont" : 17
-        },
-        {
-          "nev" : "Kar",
-          "pont" : 17
-        }
-      ],
-      "jartassagok": 
-      [
-          {
-              "nev" : "Koncentráció",
-              "pont" : 16
-          }
-      ],
-      "eletero_dobas": 
-      {
-          "dobas" : "8d8+40",
-          "eletpont ": 76
-      },
-      "kezdemenyezes": 
-      {
-          "modosito" : 1,
-          "eredet": "Ügy"
-      },
-      "vf": 
-      {
-          "ertek": 16,
-          "modositok" : 
-          [
-            [-2, "termet"], [1, "Ügy"],  [7, "természetes"]
-          ]
-      },
-      "mentok": [
-        {
-          "nev": "Szív",
-          "ertek" : 7
-        },
-        {
-          "nev": "Gyors",
-          "ertek" : 3
-        },
-        {
-          "nev": "Akarat",
-          "ertek" : 11
-        }
-      ],
-      "kihivasi_ertek": 7,
-      "tamadasok": 
-      {
-        "szam": 4,
-        "nev": "Csáp",
-        "bonusz" : 12,
-        "forma" : "kh."
-        
-      },
-      "sebzes": 
-      {
-          "nev" : "Csáp",
-          "dobas" : "1d6+9",
-          "hatas" : "Átalakítás"
-      },
-      "oldal_eleres": 
-      {
-          "szelesseg" : 10,
-          "hosszusag" : 20,
-          "tavolsag" : 10
-      },
-      "kulonleges_tamadasok": ["Átalakítás", "pszionika", "rabszolgaság"],
-      "kulonleges_kepessegek": 
-      [
-          "Nyálkafelhő"   
-      ],
-      "sebesseg": "10 láb, úszva 60 láb",
-      "kepessegek": 
-      [
-        "Éberség", "Harci Alkalmazások", "Vasakarat"        
-      ],
-      "fejlesztes": 
-      [
-          {
-              "min": 9,
-              "max": 16,
-              "meret": "Óriási"
-          },
-          {
-            "min": 17,
-            "max": 24,
-            "meret": "Hatalmas"
-          } 
-      ]
-    }
-'''
+def f():
+  return 5
 
-y = json.loads(szornyekJSON)
-print(y["fejlesztes"])
+x = reg(f)
+
+
+
+class B():
+  def __init__(self, val):
+    self.value = val
+
+  @property
+  def dyn(self):
+    return self.value // 2
+
+class A():
+  def __init__(self):
+    self.a = [B(1), B(2), B(3)]
+
+a = A()
+print (a.a[2].dyn)
